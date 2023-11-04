5a6b766f116708c6e2d6e1037bf83e80463f3df5
Arman Kumar Jena <arman.k.jena07@gmail.com>
2023-11-04 15:12:10 +0530

    custom errors added

---
+++ server/errors/index.js
@@ -0,0 +1,11 @@
+import BadRequestError from "./badRequestError";
+import NotFoundError from "./notFoundError";
+import UnAuthenticatedError from "./unAuthenticatedError";
+import UnAuthorizedError from "./unAuthorizedError";
+
+export {
+  BadRequestError,
+  NotFoundError,
+  UnAuthenticatedError,
+  UnAuthorizedError,
+};
