dcb5f09237d97c43aed047432a05fd31677cd974
Arman Kumar Jena <arman.k.jena07@gmail.com>
2023-11-11 11:30:06 +0530

    confliction error for duplicate record and custom handling err code 11000

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -4,6 +4,11 @@ const customErrorHandler = (err, req, res, next) => {
     errorCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     errorMessage: err.message || "Something went wrong ,Please try again later",
   };
+  if (err.code && err.code===11000) {
+    defaultError.errorCode=StatusCodes.CONFLICT
+    defaultError.errorMessage=`${Object.keys(err.keyValue)} field has to be unique`
+  }
   res.status(defaultError.errorCode).json({ msg: defaultError.errorMessage });
+ 
 };
 export default customErrorHandler;
